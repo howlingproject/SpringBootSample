519a0a79c782ae809da8fdb6e9f1f2197c700bce
yionlee <performizer@gmail.com>
2015-01-25 21:03:45 +0900

    applilcation add

--- src/main/java/com/libqa/application/Application.java
+++ src/main/java/com/libqa/application/Application.java
@@ -22,7 +22,6 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        System.out.println("Let's inspct the beans provider by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
